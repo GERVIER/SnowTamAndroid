8706175f09fa952fb109a3cda0be0d34f8cb6419
GERVIER <r.gervier@orange.fr>
2017-11-21 18:00:01 +0100

    Finished the SnowTam research. Started his analyse.

--- app/src/main/java/com/example/rgerv/snowtamproject/MainActivity.java
+++ app/src/main/java/com/example/rgerv/snowtamproject/MainActivity.java
@@ -7,9 +7,11 @@ import android.view.View;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.example.rgerv.snowtamproject.Model.AirportInfoRetrieving;
+import com.example.rgerv.snowtamproject.Model.SnowTam;
+import com.example.rgerv.snowtamproject.Utils.AirportInfoRetrieving;
 import com.example.rgerv.snowtamproject.Model.AirportList;
 import com.example.rgerv.snowtamproject.Model.Airport;
+import com.example.rgerv.snowtamproject.Utils.AirportSnowTamRetrieving;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -34,15 +36,17 @@ public class MainActivity extends AppCompatActivity {
                         Log.d(DebugTag, "No airport Found");
                     }
                     else{
-                        JSONObject airport = response.getJSONObject(0);
+                        JSONObject airportJson = response.getJSONObject(0);
+                        Log.d(DebugTag, airportJson.toString());
+
+                        Airport airport = new Airport(airportJson);
+
                         Log.d(DebugTag, airport.toString());
 
-                        Airport a = new Airport(airport);
-
-                        Log.d(DebugTag, a.toString());
-
-                        AirportList.getInstance().getAirportList().add(a);
+                        AirportList.getInstance().getAirportList().add(airport);
                         Log.d(DebugTag, "Airport n° " + AirportList.getInstance().getAirportList().size() + " added !");
+
+                        searchSnowTam(AirportList.getInstance().getAirportList().size()-1);
                     }
 
                 }catch (JSONException e ){
@@ -58,7 +62,48 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        AirportInfoRetrieving.RetrieveInformation("ENBO", this, responseListener, errorListener);
+        AirportInfoRetrieving.RetrieveInformation("ENZV", this, responseListener, errorListener);
+    }
+
+    public void searchSnowTam(final int airportIndex){
+        Airport airport = AirportList.getInstance().getAirportList().get(airportIndex);
+        Response.Listener<JSONArray> responseListener = new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                String code = "";
+                Boolean isSnowTamExisting = false;
+                for(int i = 0; i<response.length(); i++){
+                    try{
+                        code = response.getJSONObject(i).getString("all");
+                        if(code.contains("SNOWTAM")){
+                            isSnowTamExisting = true;
+                            break;
+                        }
+                    }catch (JSONException e){
+                        Log.d(DebugTag, "onErrorResponse: " + e.getMessage());
+                    }
+                }
+
+                if(isSnowTamExisting){
+                    Log.d(DebugTag, "Coded: " + code);
+                    Airport airport = AirportList.getInstance().getAirportList().get(airportIndex);
+                    SnowTam snowtam = new SnowTam(code);
+                    snowtam.decodeSnowTam(airport.getLocation());
+                    airport.setSnowtam(snowtam);
+                    Log.d(DebugTag, "Decoded: \n" + airport.getSnowtam().getDecodedSnowTam());
+                }
+            }
+        };
+
+        Response.ErrorListener errorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(DebugTag, "onErrorResponse: \n" + error.getMessage());
+            }
+        };
+
+
+        AirportSnowTamRetrieving.getInstance().RetrieveInformation(airport.getIcaoCode(),this,  responseListener, errorListener);
     }
 
 }
