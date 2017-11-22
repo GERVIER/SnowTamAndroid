199b70860c82df3447f364bba272e89df6eebebd
Alice <alice.delbecque.etu@univ-lemans.fr>
2017-11-23 00:41:45 +0100

    on a une petite map ici...

--- app/src/main/java/com/example/rgerv/snowtamproject/DisplayActivity.java
+++ app/src/main/java/com/example/rgerv/snowtamproject/DisplayActivity.java
@@ -38,6 +38,7 @@ public class DisplayActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_display);
         mTitle = mDrawerTitle = getTitle();
         aiportDisplayId = getIntent().getIntExtra("airportCode",0);
